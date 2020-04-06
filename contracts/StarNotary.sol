757501c0ef9bd0767789a3053cfc88affee89811
Imdad15 <imdad92@live.com>
2020-04-06 16:17:46 +0800

    add token symbol, name and star look up

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,7 +14,7 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    string public constant name = "Star Notary";
+    string public constant name = "Crypto Star Notary";
     string public constant symbol = "SN";
 
     // mapping the Star with the Owner Address
@@ -22,7 +22,7 @@ contract StarNotary is ERC721 {
     // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
-  
+    
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
         Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
@@ -58,7 +58,6 @@ contract StarNotary is ERC721 {
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
-        return tokenIdToStarInfo[_tokenId];
     }
 
     // Implement Task 1 Exchange Stars function
